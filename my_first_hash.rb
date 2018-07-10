c58f543c17ed2427e867d9845302337b0213c740
Andrew Och <andrewoch95@gmail.com>
2018-07-10 16:36:41 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -33,12 +33,14 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+  the_manifest{"muskets" => 2}
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+  the_manifest{"gun powder" => 4}
 
   # return the shipping_manifest hash below
+  
+  return the_manifest
 
 end
